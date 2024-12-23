b1eb6b6a76abb5e4eea50bd7e533bff32cb93c0a
syedaftab-dev <syed.md.aftab.2027@gmail.com>
2024-12-23 11:55:27 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+// alert(message) we r using nodejs,not browser isse browser me ek pop up atha "message" ka
+console.log( 3 + 3 )
+
+console.log("Aftab")
+
+let name = "Aftab"
+let age = 21
+let isloggedIn = false
+let state = null
+
+
+//          DATA TYPES 
+// 2^53 <--> range of number
+//  bigint  ---> big integer
+// string --> ""
+// boolean --> true/false
+// null --> standalone value
+// undefined --> 
+// symbol --> for unique
+
+
+// objects
+
+console.log(typeof null);  // object
+console.log(typeof undefined)  // undefined
