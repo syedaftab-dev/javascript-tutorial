7cf3e2ada9fba9d0c5396efa9a22bd5121b9162b
syedaftab-dev <syed.md.aftab.2027@gmail.com>
2024-12-27 04:19:52 +0000

    date completed

---
+++ 01_basic/07_date_time.js
@@ -0,0 +1,18 @@
+// Date 
+
+let myDate = new Date();
+console.log(myDate);
+console.log(myDate.toString());   
+console.log(myDate.toLocaleString());  // date format changed
+console.log(myDate.toLocaleDateString());  // gives only date modified of above one
+
+//                      yr   month   date  hour   min
+// let newDate = new Date(2023,  0   ,24    , 5    , 30)
+let newDate = new Date("12-27-2024")
+console.log(newDate.toLocaleString());
+
+console.log(newDate.toLocaleString('default',{
+    weekday:"long",
+}));
+
+
